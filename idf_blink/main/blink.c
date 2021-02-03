4c0225c0d5f2175f0f14a2feb60a39e0965cd460
Andrea Toninelli <toninelli.andrea@aerauliqa.it>
2021-02-03 16:33:54 +0100

    terzo commit (test)

--- idf_blink/main/blink.c
+++ idf_blink/main/blink.c
@@ -32,10 +32,10 @@ void app_main(void)
         /* Blink off (output low) */
 	printf("Turning off the LED\n");
         gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
 	printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
