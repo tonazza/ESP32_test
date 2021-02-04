e61a9c007683f3784c76bb66cca2b24bd74c8017
Andrea Toninelli <tonazza@yahoo.it>
2021-02-04 11:11:32 +0100

    dodicesimo

--- idf_blink/main/blink.c
+++ idf_blink/main/blink.c
@@ -16,8 +16,9 @@
    or you can edit the following line and set a number here.
    commento a caso
 */
-#define BLINK_GPIO 2
-/* 2 = verde 4 = rosso */
+#define BLINK_GPIO 4
+/* 2 = verde
+ * 4 = rosso */
 
 void app_main(void)
 {
